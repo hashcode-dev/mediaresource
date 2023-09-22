ac7d490d27f9d5bbf4ead2103f132e124460df2d
Gaurav Sahu <hashcode.dev@gmail.com>
2023-09-22 19:53:20 +0530

    Environment Variable Changes

--- cloud_env.sh
+++
@@ -1,8 +0,0 @@
-printenv > .env
-
-echo "MONGODB_URL = $MONGODB_URL"
-
-sed -i 's/%MONGODB_URL%/'"$MONGODB_URL"'/g' app.yaml
-sed -i 's/%MONGODB_COLLECTION%/'"$MONGODB_COLLECTION"'/g' app.yaml
-
-cat app.yaml
